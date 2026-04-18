93a8a2b0ddf2b6a6ca6aab0c727b34b2da09f81f
Zac-Smucker-Bryan <zacary.bryan@gmail.com>
2026-04-18 12:29:19 -0500

    Moving my component to the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PizzaToppings from './PizzaToppings';
+import ZsmuckerbryanComponent from './ZsmuckerbryanComponent';
 import TsteeleComponent from "./TsteeleComponent";
 import Welcome from "./Welcome";
 import ThamiltonComponent from "./ThamiltonComponent";
 import DweiseComponent from './DweiseAccordionComponent';
 import CBaresComponent from './CBaresComponent';
-import ZsmuckerbryanComponent from './ZsmuckerbryanComponent';
 
 
 const App = () => {
   return (
     <div className="m-3">
       <h1>React Components</h1>
+      <ZsmuckerbryanComponent />
       <PizzaToppings />
       <TsteeleComponent />
       <Welcome />
       <ThamiltonComponent />
       <DweiseComponent />
       <CBaresComponent />
-      <ZsmuckerbryanComponent />
     </div>
   );
 };
